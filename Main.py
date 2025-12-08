75f0eb170eb5b92b8f8bea5757ff64ef162d8c2d
Richard Prestl <rprestl1712@gmail.com>
2025-12-08 14:18:00 +0100

    start

---
+++ Main.py
@@ -0,0 +1,58 @@
+import sys
+import numpy as np
+import time
+
+#Print Delay
+
+
+
+
+# import sys
+#
+# import pygame
+#
+# global active, i
+# pygame.init()
+#
+# SCREEN = pygame.display.set_mode((1008, 720))
+# pygame.display.set_caption("Mons")
+# typ = ["ohen", "elektrina", "voda"]
+# ability = ["sunny day"]
+# moves = ["move1", "move2", "move3", "move4"]
+# resistance = [typ[0]]
+# weakness = [typ[1]]
+# Charizard = ["Charizard", typ[0], ability[0], resistance[0], weakness[0], moves[0], moves[1], moves[2], moves[3]]
+# mon = [Charizard, "Pikachu"]
+# battlestart = True
+# batle = False
+#
+#
+# def battle():
+#     print("Vyslal si " + active)
+#     print(" ")
+#     print("1. " + active[0][-4])
+#     print("2. " + active[0][-3])
+#     print("3. " + active[0][-2])
+#     print("4. " + active[0][-1])
+#     print("5. Vymen")
+#     while True:
+#         pass
+#
+#
+# print("Vyber si mona:")
+# print(" ")
+# print("1" + " " + mon[0][0])
+# print("2" + " " + mon[1])
+# print("3")
+# print("4")
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             sys.exit()
+#         if event.type == pygame.KEYDOWN:
+#             if event.key == pygame.K_1:
+#                 active = mon[0][0]
+#                 battle()
+#             if event.key == pygame.K_2:
+#                 active = mon[1]
+#                 battle()
