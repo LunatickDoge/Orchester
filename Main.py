7fc67962568661563441a82e5ba87069ea1ba1cd
Richard Prestl <rprestl1712@gmail.com>
2025-12-15 14:57:46 +0100

    start

--- Main.py
+++ Main.py
@@ -1,10 +1,25 @@
-import sys
 import time
+import pygame
+import sys
+from button import Button
+
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((1008, 720))
+pygame.display.set_caption("Space Invaders")
+BG = pygame.image.load('assets/Background.png')
+bg_game = pygame.image.load('assets/backstar2.jpg')
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+hudba = 1
+
+
+def get_font(size):
+    return pygame.font.Font("assets/font.ttf", size)
 
 
 # Print Delay
-
-
 def delay_print(s):
     # 1pismeno naraz
 
@@ -18,15 +33,17 @@ def delay_print(s):
 
 
 class Mon:
-    def __init__(self, name, types, moves, ev, health="===================="):
+    def __init__(self, name, types, moves, ev, hp, health="===================="):
         self.name = name
         self.types = types
         self.moves = moves
         self.attack = ev["ATTACK"]
         self.defense = ev["DEFENSE"]
+        self.speed = ev["SPEED"]
         self.bars = 20
         self.health = health
-
+        self.hp = hp
+        self.max = hp
     def fight(self, pokemon2):
         global string_1_attack, supeff, string_2_attack
         print("BATTLE START")
@@ -48,26 +65,26 @@ class Mon:
             supeff = True
             if self.types == k:
                 if pokemon2.types == k:
-                    string_1_attack = "It's not very effective..."
-                    string_2_attack = "It's not very effective..."
+                    string_1_attack = "\nIt's not very effective..."
+                    string_2_attack = "\nIt's not very effective..."
             # Pokemon 2 ma Type advantage
-            elif pokemon2.types == version[(i + 1) % 3]:
+            elif pokemon2.types == version[(i + 2) % 3]:
                 pokemon2.attack *= 2
                 pokemon2.defense *= 2
-                string_1_attack = "It's not very effective"
-                string_2_attack = "It's SUPER effective"
+                string_1_attack = "\nIt's not very effective!"
+                string_2_attack = "\nIt's SUPER effective!"
             # self ma Type advantage
-            elif pokemon2.types == version[(i + 2) % 3]:
+            elif pokemon2.types == version[(i + 1) % 3]:
                 self.attack *= 2
                 self.defense *= 2
-                string_1_attack = "It's SUPER effective"
-                string_2_attack = "It's not very effective"
+                string_1_attack = "\nIt's SUPER effective!"
+                string_2_attack = "\nIt's not very effective!"
             else:
                 supeff = False
 
         # Fight:
 
-        while (0 < self.bars) and (pokemon2.bars > 0):
+        while (0 < self.hp) and (pokemon2.hp > 0):
             print(f"{self.name}\t\tHP\t{self.bars}")
             print(f"{pokemon2.name}\t\tHP\t{pokemon2.bars}")
 
@@ -81,9 +98,9 @@ class Mon:
                 delay_print(string_1_attack)
 
             # Damage
-            pokemon2.bars -= self.attack
+            pokemon2.hp -= self.attack
             pokemon2.health = ""
-            for j in range(int(pokemon2.bars + .1 * pokemon2.defense)):
+            for j in range(int(pokemon2.hp / pokemon2.max * 20 + .1 * pokemon2.defense)):
                 pokemon2.health += "="
 
             time.sleep(1)
@@ -106,25 +123,125 @@ class Mon:
 
             self.bars -= pokemon2.attack
             self.health = ""
-            for j in range(int(pokemon2.bars + .1 * pokemon2.defense)):
+            for j in range(int(self.hp / self.max * 20 + .1 * self.defense)):
                 self.health += "="
 
             time.sleep(1)
-            print(f"{self.name}\t\tHP\t{self.health}")
-            print(f"{pokemon2.name}\t\tHP\t{pokemon2.health}")
+            print(f"\n{self.name}\t\tHP\t{self.health}")
+            print(f"\n{pokemon2.name}\t\tHP\t{pokemon2.health}")
 
-            if self.bars <= 0:
+            if self.hp <= 0:
                 delay_print("\n..." + self.name + " fainted.")
                 break
 
 
 if __name__ == "__main__":
     Charizard = Mon("Charizard", "Fire", ["Flamethrower", "Fly", "Blast Burn", "Firepunch"],
-                    {"ATTACK": 12, "DEFENSE": 6, "SPEED": 10})
+                    {"ATTACK": 12, "DEFENSE": 6, "SPEED": 10}, 40)
     Blastoise = Mon("Blastoise", "Water", ["Bubblebeam", "Tackle", "Headbutt", "Surf"],
-                    {"ATTACK": 12, "DEFENSE": 6, "SPEED": 10})
+                    {"ATTACK": 12, "DEFENSE": 6, "SPEED": 10}, 55)
     Venusaur = Mon("Venusaur", "Grass", ["Solar Beam", "Giga Drain", "Sleep Powder", "Leech Seed"],
-                    {"ATTACK": 12, "DEFENSE": 6, "SPEED": 10})
+                   {"ATTACK": 12, "DEFENSE": 6, "SPEED": 10}, 70)
+
+
+def play():
+    pygame.mixer.music.play(-1)
+    Charizard.fight(Blastoise)
+
+
+def options():
+    global hudba
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+
+        OPTIONS_TEXT = get_font(35).render("Vyber si hudbu:", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(504, 260))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        ASGORE = Button(image=None, pos=(504, 360),
+                        text_input="DEFAULT", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        TRUEHERO = Button(image=None, pos=(504, 460),
+                          text_input="ALTERNATIVE", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        SANS = Button(image=None, pos=(504, 560),
+                      text_input="MEME", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        ASGORE.changecolor(OPTIONS_MOUSE_POS)
+        ASGORE.update(SCREEN)
+
+        TRUEHERO.changecolor(OPTIONS_MOUSE_POS)
+        TRUEHERO.update(SCREEN)
+
+        SANS.changecolor(OPTIONS_MOUSE_POS)
+        SANS.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if ASGORE.checkforinput(OPTIONS_MOUSE_POS):
+                    hudba = 1
+                    main_menu()
+                if TRUEHERO.checkforinput(OPTIONS_MOUSE_POS):
+                    hudba = 2
+                    main_menu()
+                if SANS.checkforinput(OPTIONS_MOUSE_POS):
+                    hudba = 3
+                    main_menu()
+
+        pygame.display.update()
+
+
+def main_menu():
+    if hudba == 1:
+        pygame.mixer.music.load('../Mojahra/872882_Undertale-Asgore-Theme.mp3')
+    if hudba == 2:
+        pygame.mixer.music.load('../Mojahra/656569_Undertale-Undyne-the-Undyi.mp3')
+    if hudba == 3:
+        pygame.mixer.music.load('695513_-Megalovania--Remix.mp3')
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(504, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(504, 250),
+                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(504, 400),
+                                text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(504, 550),
+                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changecolor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkforinput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkforinput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkforinput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+
+main_menu()
+
 
 # import sys
 #
